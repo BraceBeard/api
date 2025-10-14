5d0f082dcb3dedd5cbe75821a6325b27f1c24538
Eduard Ramirez <edd.ramirez@proton.me>
2025-10-14 07:34:57 -0500

    Introduce shared router and wire routes in modules

--- src/home.ts
+++ src/home.ts
@@ -1,3 +1,7 @@
-export function homeRouteHandler (_req: Request): Response {
-  return new Response("It's working!");
+import { router } from "./shared/index.ts";
+
+export function homeRouteHandler(_req: Request): Response {
+	return new Response("It's working!");
 }
+
+router.addRoute("/", homeRouteHandler);
