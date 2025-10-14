bd6fabc1763cf85ee739e6ef537c4c5c15fb2718
Eduard Ramirez <edd.ramirez@proton.me>
2025-10-14 08:40:43 -0500

    Delete test route file

--- src/routes/test.ts
+++
@@ -1,7 +0,0 @@
-import { router } from "../shared/index.ts";
-
-export function TestRouteHandler(_req: Request): Response {
-	return new Response("Test page: " + Deno.env.get("PRUEBA"));
-}
-
-router.addRoute("/test", TestRouteHandler);
