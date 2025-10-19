ad506ffedb4bc048f124e710fcb8b5de4257b893
Eduard Ramirez <edd.ramirez@proton.me>
2025-10-18 19:13:24 -0500

    chore: rename db directory to .db and update related paths

--- core/shared/index.ts
+++ core/shared/index.ts
@@ -2,5 +2,5 @@ import { Router } from "../../core/router.ts";
 import * as path from "@std/path";
 
 export const router = new Router();
-const dbPath = path.join(Deno.cwd(), "db", "data.sqlite3");
+const dbPath = path.join(Deno.cwd(), ".db", "data.sqlite3");
 export const kv = await Deno.openKv(dbPath);
